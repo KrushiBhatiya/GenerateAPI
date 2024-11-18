b7562efc8070bea953b3ef00d5d39e09b50c0c8a
Krushi-bhatiya <krushi5503@gmail.com>
2024-11-18 14:13:51 +0530

    demo

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ var AC = require('../controller/apiController')
 
 // var st = "hello"
 router.post('/:name' , TC.checkToken , AC.createData)
+router.post('/:name/:id' , TC.checkToken , AC.editData)
 router.get('/:name' , TC.checkToken , AC.viewData)
 router.delete('/:name/:id' , TC.checkToken , AC.deleteData)
 router.patch('/:name/:id', TC.checkToken , AC.editData)
